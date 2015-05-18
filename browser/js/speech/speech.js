b9e2c6ea57504547fc15b64b36846c972b4cdf5c
darksim735 <darksim735@gmail.com>
2015-05-18 03:20:17 -0400

    Speech recognition and continuous update works, but does not yet display in divs. Only console logs

--- browser/js/speech/speech.js
+++ browser/js/speech/speech.js
@@ -18,4 +18,44 @@ $scope.test = "HELLO THIS WORKED";
 
 
 
+if (!('webkitSpeechRecognition' in window)) {
+    //handle error stuff here...
+} else {
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.start();
+
+    var final_transcript = '';
+
+    recognition.onresult = function (event) {
+        var interim_transcript = '';
+
+        if (typeof (event.results) == 'undefined') {
+            recognition.onend = null;
+            recognition.stop();
+            upgrade();
+            return;
+        }
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+
+            } else {
+            	console.log("Interim Transcript:", interim_transcript);
+                interim_transcript += event.results[i][0].transcript;
+            }
+        }
+        console.log("Final Transcript:", interim_transcript);
+//        document.getElementsByTagName('div')[0].innerText = final_transcript;
+        $scope.test = final_transcript;
+
+    };
+
+
+}
+
+
+
 });
